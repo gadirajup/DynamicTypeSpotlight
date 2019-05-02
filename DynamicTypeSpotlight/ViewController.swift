e96907581787cad7c28e28424f807688893adc97
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-01 22:01:01 -0700

    Core Spotlight

--- DynamicTypeSpotlight/ViewController.swift
+++ DynamicTypeSpotlight/ViewController.swift
@@ -8,15 +8,27 @@
 
 import UIKit
 import SafariServices
+import CoreSpotlight
+import MobileCoreServices
 
 class ViewController: UITableViewController {
     
     var projects = [[String]]()
+    var favorites = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupProjects()
+        
+        let defaults = UserDefaults.standard
+        
+        if let savedFavorites = defaults.object(forKey: "favorites") as? [Int] {
+            favorites = savedFavorites
+        }
+        
+        tableView.isEditing = true
+        tableView.allowsSelectionDuringEditing = true
     }
     
     
@@ -49,6 +61,12 @@ extension ViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         
+        if favorites.contains(indexPath.row) {
+            cell.editingAccessoryType = .checkmark
+        } else {
+            cell.editingAccessoryType = .none
+        }
+        
         let attributedString = NSMutableAttributedString(string: "\(projects[indexPath.row][0])\n", attributes: [NSAttributedString.Key.font : UIFont.preferredFont(forTextStyle: .headline), NSAttributedString.Key.foregroundColor: UIColor.red])
         attributedString.append(NSAttributedString(string: projects[indexPath.row][1], attributes: [NSAttributedString.Key.font : UIFont.preferredFont(forTextStyle: .subheadline)]))
         
@@ -59,5 +77,69 @@ extension ViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         showProject(indexPath.row)
     }
+    
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        if favorites.contains(indexPath.row) {
+            return .delete
+        } else {
+            return .insert
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .insert {
+            favorites.append(indexPath.row)
+            index(item: indexPath.row)
+        } else {
+            if let index = favorites.firstIndex(of: indexPath.row) {
+                favorites.remove(at: index)
+                deindex(item: indexPath.row)
+            }
+        }
+        
+        let defaults = UserDefaults.standard
+        defaults.set(favorites, forKey: "favorites")
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    func index(item: Int) {
+        let project = projects[item]
+        
+        let attributeSet = CSSearchableItemAttributeSet(itemContentType: kUTTypeText as String)
+        attributeSet.title = project[0]
+        attributeSet.contentDescription = project[1]
+        
+        let item = CSSearchableItem(uniqueIdentifier: "\(item)", domainIdentifier: "me.prudhvi", attributeSet: attributeSet)
+        
+        CSSearchableIndex.default().indexSearchableItems([item]) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("Indexed")
+            }
+        }
+    }
+    
+    func deindex(item: Int) {
+        
+        CSSearchableIndex.default().deleteSearchableItems(withIdentifiers: ["\(item)"]) { (error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else {
+                print("De-indexed successfully")
+            }
+        }
+        
+    }
+    
+//    override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+//        let action = UITableViewRowAction(style: .normal, title: "Woo") { (action, indexPath) in
+//            print(indexPath.row)
+//        }
+//        action.backgroundColor = .purple
+//        return [action]
+//    }
 }
 
