cc8769f396d53dd2cc1f3235ea243af4e6e2be0e
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2019-05-01 21:11:22 -0700

    tableview

--- DynamicTypeSpotlight/ViewController.swift
+++ DynamicTypeSpotlight/ViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 
 class ViewController: UITableViewController {
+    
+    var projects = [[String]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupProjects()
     }
+    
+    
+    private func setupProjects() {
+        projects.append(["Project 1: Storm Viewer", "Constants and variables, UITableView, UIImageView, FileManager, storyboards"])
+        projects.append(["Project 2: Guess the Flag", "@2x and @3x images, asset catalogs, integers, doubles, floats, operators (+= and -=), UIButton, enums, CALayer, UIColor, random numbers, actions, string interpolation, UIAlertController"])
+        projects.append(["Project 3: Social Media", "UIBarButtonItem, UIActivityViewController, the Social framework, URL"])
+        projects.append(["Project 4: Easy Browser", "loadView(), WKWebView, delegation, classes and structs, URLRequest, UIToolbar, UIProgressView., key-value observing"])
+        projects.append(["Project 5: Word Scramble", "Closures, method return values, booleans, NSRange"])
+        projects.append(["Project 6: Auto Layout", "Get to grips with Auto Layout using practical examples and code"])
+        projects.append(["Project 7: Whitehouse Petitions", "JSON, Data, UITabBarController"])
+        projects.append(["Project 8: 7 Swifty Words", "addTarget(), enumerated(), count, index(of:), property observers, range operators."])
+    }
+}
 
-
+extension ViewController {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return projects.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(projects[indexPath.row][0]) - \(projects[indexPath.row][1])"
+        return cell
+    }
 }
 
